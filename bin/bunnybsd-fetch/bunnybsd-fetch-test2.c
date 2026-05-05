4f64387c42d8900849e669ea1449155bec0bf48e
jyomama28 <jyomama28@gmail.com>
2026-05-04 22:00:58 -0400

    Update bunnybsd-fetch-test2.c

--- bin/bunnybsd-fetch/bunnybsd-fetch-test2.c
+++ bin/bunnybsd-fetch/bunnybsd-fetch-test2.c
@@ -65,8 +65,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-printf("BunnyBSD Verison 1.0.0\n");
+int main(int argc, char *argv[]) {
+FILE *fp;
+char path[1035]
+fp = popen("bunnybsd-version");
+if (fp == NULL) {
+printf("Failed to run command\n");
+exit(1);
+}
+while (fgets(path, sizeof(path), fp) != NULL) {
+printf("%s", path);
+}
+pclose(fp);
+printf("BunnyBSD: Security with a cute bunny.\n");
 printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠙⢦⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 printf("⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⢀⡼⠋⠁⠀⣙⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 printf("⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⡰⠋⠀⢀⡴⢋⡽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
